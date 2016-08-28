bd84d82cbde71eaae35e3cf15d483efa3fa356da
crellison <cole.ellison@gmail.com>
2016-08-28 16:50:55 -0400

    now you can send data

--- errors.js
+++ errors.js
@@ -4,13 +4,14 @@
 var codes = require('./errors.json');
 
 // error handler message, sends error messages in json
-module.exports = function(res,err,message) {
+module.exports = function(res,err,message,data) {
 		err = parseInt(err);
 		if (isNaN(err) || codes[err] === undefined) 
 			throw Error("Don't go breaking the error handler");
 		message = message || codes[err];
-		return res.status(err).send({
+		return res.status(err).json({
 			success: (err < 400 ? true : false),
-			message: message
+			message: message,
+			data: data
 		});
 	}
